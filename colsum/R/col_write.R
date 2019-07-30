c8f8b6b78ab9369e53c7cf5c2680b21207c6dc1a
Leonardo Collado Torres <lcollado@jhsph.edu>
2019-07-30 11:03:50 -0500

    fix some translations

--- colsum/R/col_write.R
+++ colsum/R/col_write.R
@@ -1,2 +1,15 @@
 col_write <- function(df, path = tempdir()) {
+
+  col_names <- colnames(df)
+  file_paths <- map_chr(
+    col_names,
+    ~ file.path(path, paste0(.x, '.txt'))
+  )
+
+  # .x <- df[[1]]
+  # .y <- file_paths[[1]]
+  # writeLines(as.character(.x), .y)
+
+
+  walk2(df, file_paths, ~ writeLines(as.character(.x), .y))
 }
